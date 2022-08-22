81cb2f65b204cdd1818923c141663c612b8ddec9
rozdersteam <juanromero011@gmail.com>
2022-08-22 09:35:45 -0700

    update gangs

--- shared/gangs.lua
+++ shared/gangs.lua
@@ -4,7 +4,7 @@ QBShared.Gangs = {
 		label = 'No Gang',
 		grades = {
             ['0'] = {
-                name = 'Unaffiliated'
+                name = 'Unaffiliated',
             },
         },
 	},
@@ -12,16 +12,35 @@ QBShared.Gangs = {
 		label = 'The Lost MC',
 		grades = {
             ['0'] = {
-                name = 'Recruit'
+                name = 'Nomad'
             },
 			['1'] = {
-                name = 'Enforcer'
+                name = 'Prospect'
             },
 			['2'] = {
-                name = 'Shot Caller'
+                name = 'Loyal'
             },
 			['3'] = {
-                name = 'Boss',
+                name = 'Enforcer',
+            },
+            ['4'] = {
+                name = 'Sergeant at Arms',
+            },
+            ['5'] = {
+                name = 'Road Captain',
+            },
+            ['6'] = {
+                name = 'Treasurer',
+            },
+            ['7'] = {
+                name = 'Secretary',
+            },
+            ['8'] = {
+                name = 'Vice President',
+				isboss = true
+            },
+            ['9'] = {
+                name = 'President',
 				isboss = true
             },
         },
@@ -30,13 +49,13 @@ QBShared.Gangs = {
 		label = 'Ballas',
 		grades = {
             ['0'] = {
-                name = 'Recruit'
+                name = 'Recruit',
             },
 			['1'] = {
-                name = 'Enforcer'
+                name = 'Enforcer',
             },
 			['2'] = {
-                name = 'Shot Caller'
+                name = 'Shot Caller',
             },
 			['3'] = {
                 name = 'Boss',
@@ -44,20 +63,62 @@ QBShared.Gangs = {
             },
         },
 	},
-	['vagos'] = {
-		label = 'Vagos',
+    ['tapscrime'] = {
+		label = 'Taps Crime',
 		grades = {
             ['0'] = {
-                name = 'Recruit'
+                name = 'Associate',
             },
 			['1'] = {
-                name = 'Enforcer'
+                name = 'Soldier',
             },
 			['2'] = {
-                name = 'Shot Caller'
+                name = 'Underboss',
+                isboss = true
             },
 			['3'] = {
-                name = 'Boss',
+                name = 'Consigliere',
+				isboss = true
+            },
+            ['4'] = {
+                name = 'Boss/Don',
+				isboss = true
+            },
+        },
+	},
+    ['shatteredangels'] = {
+		label = 'Shattered Angels',
+		grades = {
+            ['0'] = {
+                name = 'Street Hustler',
+            },
+			['1'] = {
+                name = 'Street Soldier',
+            },
+			['2'] = {
+                name = 'Turf-Area Leader',
+            },
+			['3'] = {
+                name = 'Gang Leader',
+				isboss = true
+            },
+        },
+	},
+	['chaingang'] = {
+		label = 'Chain Gang',
+		grades = {
+            ['0'] = {
+                name = 'Gangster',
+            },
+			['1'] = {
+                name = 'Enforcer',
+            },
+			['2'] = {
+                name = 'Captain',
+                isboss = true
+            },
+			['3'] = {
+                name = 'OG',
 				isboss = true
             },
         },
@@ -66,13 +127,13 @@ QBShared.Gangs = {
 		label = 'Cartel',
 		grades = {
             ['0'] = {
-                name = 'Recruit'
+                name = 'Recruit',
             },
 			['1'] = {
-                name = 'Enforcer'
+                name = 'Enforcer',
             },
 			['2'] = {
-                name = 'Shot Caller'
+                name = 'Shot Caller',
             },
 			['3'] = {
                 name = 'Boss',
@@ -84,13 +145,13 @@ QBShared.Gangs = {
 		label = 'Families',
 		grades = {
             ['0'] = {
-                name = 'Recruit'
+                name = 'Recruit',
             },
 			['1'] = {
-                name = 'Enforcer'
+                name = 'Enforcer',
             },
 			['2'] = {
-                name = 'Shot Caller'
+                name = 'Shot Caller',
             },
 			['3'] = {
                 name = 'Boss',
@@ -102,18 +163,98 @@ QBShared.Gangs = {
 		label = 'Triads',
 		grades = {
             ['0'] = {
-                name = 'Recruit'
+                name = 'Recruit',
             },
 			['1'] = {
-                name = 'Enforcer'
+                name = 'Enforcer',
             },
 			['2'] = {
-                name = 'Shot Caller'
+                name = 'Shot Caller',
             },
 			['3'] = {
                 name = 'Boss',
 				isboss = true
             },
         },
-	}
+	},
+    ['bloods'] = {
+		label = 'Bloods',
+		grades = {
+            ['0'] = {
+                name = 'LYB',
+            },
+            ['1'] = {
+                name = 'YB',
+            },
+            ['2'] = {
+                name = 'OYB',
+            },
+            ['3'] = {
+                name = 'OB',
+                isboss = true
+            },
+			['4'] = {
+                name = 'OOB',
+                isboss = true
+            },
+			['5'] = {
+                name = 'OOOB',
+                isboss = true
+            },
+        },
+	},
+    ['crips'] = {
+		label = 'Crips',
+		grades = {
+            ['0'] = {
+                name = 'Young Gangster',
+            },
+            ['1'] = {
+                name = 'Original Young Gangster',
+            },
+            ['2'] = {
+                name = 'Original Baby Gangster',
+            },
+            ['3'] = {
+                name = 'Original Gangster',
+                isboss = true
+            },
+			['4'] = {
+                name = 'Triple Original Gangster',
+                isboss = true
+            },
+        },
+	},
+    ['roadwolfs'] = {
+		label = 'Road Wolves MC',
+		grades = {
+            ['0'] = {
+                name = 'Prospect',
+            },
+            ['1'] = {
+                name = 'Patched Member',
+            },
+            ['2'] = {
+                name = 'Tail Gunner',
+            },
+            ['3'] = {
+                name = 'Road Captain',
+            },
+			['4'] = {
+                name = 'Enforcer',
+            },
+            ['5'] = {
+                name = 'Sergeant at Arms',
+                isboss = true
+            },
+            ['6'] = {
+                name = 'Vice President',
+                isboss = true
+            },
+            ['7'] = {
+                name = 'President',
+                isboss = true
+            },
+        },
+	},
 }
